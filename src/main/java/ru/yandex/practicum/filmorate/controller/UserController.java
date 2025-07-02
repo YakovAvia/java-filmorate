ccd115779c0fb6204112997b775a5a6b48105643
YakovAvia <ralfffi@yandex.ru>
2025-07-02 09:01:38 +0300

    java-filmorate #3

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 @RestController
 public class UserController {
 
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     private final List<User> users = new ArrayList<>();
 
